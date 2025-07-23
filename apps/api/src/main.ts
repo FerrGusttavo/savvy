cd48dbe57e5fef7a5a9c78f68d2a2155ea3c9181
FerrGusttavo <gustavo.augusto.f@outlook.com>
2025-07-23 10:56:55 -0300

    chore: environment configuration and Swagger integration

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
+import { EnvService } from './env/env.service'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(process.env.PORT ?? 3000)
+
+  const envService = app.get(EnvService)
+  const PORT = envService.get('PORT')
+
+  const config = new DocumentBuilder()
+    .setTitle('Savvy API')
+    .setDescription('API documentation for Savvy')
+    .setVersion('1.0')
+    .addTag('savvy')
+    .build()
+  const documentFactory = () => SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, documentFactory())
+
+  await app.listen(PORT)
 }
 bootstrap()
